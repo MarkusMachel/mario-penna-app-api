d8197b0360c71cdbd9fc598a378de68482e63bbe
Markus Machel <machelmarkus@gmail.com>
2023-05-18 19:57:44 -0300

    crud atividade

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-//Route::get('/atividade/{id}', [AtividadeController::class, 'getAtividade']);
-Route::get('/atividade/{id}', 'App\Http\Controllers\AtividadeController@show')->name('atividade.show');
 
-Route::post('/atividade', 'App\Http\Controllers\AtividadeController@store')->name('atividade.store');
+//criar uma rota para buscar uma atividade por matching de palavra
+//Route::get...
+
+Route::get('/atividade/{id}', 'App\Http\Controllers\AtividadeController@buscarAtividade')->name('atividade.buscarAtividade');
+
+Route::post('/atividade', 'App\Http\Controllers\AtividadeController@salvarAtividade')->name('atividade.salvarAtividade');
 
 
 Route::get('/test', function () {
